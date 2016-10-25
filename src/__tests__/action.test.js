b62a52dcdc9ca9454c474c0ed954fea260088f93
kuwood <p4nic510@gmail.com>
2016-10-25 12:43:12 -0600

    Add action tests

---
+++ src/__tests__/action.test.js
@@ -0,0 +1,72 @@
+import * as videoActions from '../actions/videoActions'
+import * as userActions from '../actions/userActions'
+import * as roomActions from '../actions/roomActions'
+
+
+describe('videoActions', () => {
+  it('should create an action to pause the video', () => {
+    const expectedAction = {
+      type: videoActions.PAUSE_VIDEO,
+      playing: false
+    }
+    expect(videoActions.pauseVideo()).toEqual(expectedAction)
+  })
+  it('should create an action to play the video', () => {
+    const expectedAction = {
+      type: videoActions.PLAY_VIDEO,
+      playing: true
+    }
+    expect(videoActions.playVideo()).toEqual(expectedAction)
+  })
+  it('should create an action to set the video position', () => {
+    const position = 30000
+    const expectedAction = {
+      type: videoActions.SET_POSITION,
+      position: position
+    }
+    expect(videoActions.setPosition(position)).toEqual(expectedAction)
+  })
+})
+
+describe('userActions', () => {
+  it('should create an action to set requestPause to false', () => {
+    const expectedAction = {
+      type: userActions.REQUEST_PLAY_VIDEO,
+      requestPause: false
+    }
+    expect(userActions.requestPlayVideo()).toEqual(expectedAction)
+  })
+  it('it should create an action to set requestPause to true', () => {
+    const expectedAction = {
+      type: userActions.REQUEST_PAUSE_VIDEO,
+      requestPause: true
+    }
+    expect(userActions.requestPauseVideo()).toEqual(expectedAction)
+  })
+  it('should create an action to set user as being ready', () => {
+    const expectedAction = {
+      type: userActions.IS_READY,
+      requestPause: false,
+      isReady: true
+    }
+    expect(userActions.isReady()).toEqual(expectedAction)
+  })
+  it('should create an action to set user as not ready', () => {
+    const expectedAction = {
+      type: userActions.IS_NOT_READY,
+      isReady: false
+    }
+    expect(userActions.isNotReady()).toEqual(expectedAction)
+  })
+})
+
+describe('roomActions', () => {
+  it('should create an action to set the room to ready', () => {
+    const bool = true
+    const expectedAction = {
+      type: roomActions.ROOM_READY,
+      isReady: bool
+    }
+    expect(roomActions.roomReady(bool)).toEqual(expectedAction)
+  })
+})
--- src/actions/roomActions.js
+++ src/actions/roomActions.js
@@ -1,7 +1,6 @@
 export const ROOM_READY = 'ROOM_READY'
 
 export const roomReady = (bool) => {
-  console.log(bool);
   return {
     type: ROOM_READY,
     isReady: bool
