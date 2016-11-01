b28295f69115590461b282102628da13d036125b
kuwood <p4nic510@gmail.com>
2016-11-01 17:38:44 -0600

    Remove well from chat, Add border color to chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
-import { Well, Panel } from 'react-bootstrap'
+import { Panel } from 'react-bootstrap'
 
 class Chat extends Component {
   render () {
     return (
-      <Panel header="CHAT">
-        <Well>
-          <div className="chat-container">
-            <p>Chatter: chat chat chat</p>
-          </div>
-        </Well>
+      <Panel header="CHAT" bsStyle="info">
+        <div className="chat-container">
+          <p>Chatter: chat chat chat</p>
+        </div>
       </Panel>
     )
   }
