ee3095352a28268e51db2c0532b561457d11eef3
Keith Underwood <keithunderwood510@gmail.com>
2017-06-20 15:27:08 -0700

    Move app directions to its own component

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { socket } from '../index'
 import { Jumbotron, Button } from 'react-bootstrap'
 import Logo from './Logo'
+import Directions from './Directions'
 
 export class Jumbo extends Component {
   constructor(props) {
@@ -23,34 +24,7 @@ export class Jumbo extends Component {
               <Button type="button" bsStyle="custom" onClick={this.createRoom}>Create a room</Button>
             </Jumbotron>
           </div>
-          <div className="directions-wrap">
-            <div>
-              <div className="directions-heading">
-                <h3>Dont bother doing a "count down".</h3>
-                <p className="directions-text">Simplify YouTube Synchronization in three easy steps.</p>
-              </div>
-            </div>
-            <div className="directions-container">
-              <div className="directions-block">
-                <p className="directions-title">Create a room.</p>
-                <p className="directions-text">Use the create a room button to get started with a generated room i.d. or click join room to create
-                  one with a custom name.
-                </p>
-              </div>
-              <div className="directions-block">
-                <p className="directions-title">Get a youtube URL.</p>
-                <p className="directions-text">Find a youtube video to share with the room, grab the url and use the "Set input video" button to set the
-                  video for the room.
-                </p>
-              </div>
-              <div className="directions-block">
-                <p className="directions-title">Enjoy the video.</p>
-                <p className="directions-text">Once you have set the video, you will need to wait until all users in the room have flagged
-                  themselves as "ready". Once everyone is ready you can hit play!
-                </p>
-              </div>
-            </div>
-          </div>
+          <Directions />
         </div>
       </div>
     )
