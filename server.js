bbd8d48129ac1143dcced039c20105bd061df4a3
Keith Underwood <keithunderwood510@gmail.com>
2017-06-26 19:10:42 -0700

    Remove includes polyfill

--- server.js
+++ server.js
@@ -1,41 +1,6 @@
 /*eslint no-console: */
 // eslint-disable-next-line
 'use strict'
-// .includes polyfill
-if (!Array.prototype.includes) {
-  // eslint-disable-next-line
-  Array.prototype.includes = function(searchElement /*, fromIndex*/) {
-    if (this == null) {
-      throw new TypeError('Array.prototype.includes called on null or undefined');
-    }
-
-    var O = Object(this);
-    var len = parseInt(O.length, 10) || 0;
-    if (len === 0) {
-      return false;
-    }
-    var n = parseInt(arguments[1], 10) || 0;
-    var k;
-    if (n >= 0) {
-      k = n;
-    } else {
-      k = len + n;
-      if (k < 0) {k = 0;}
-    }
-    var currentElement;
-    while (k < len) {
-      currentElement = O[k];
-      if (searchElement === currentElement ||
-         // eslint-disable-next-line
-         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-        return true;
-      }
-      k++;
-    }
-    return false;
-  };
-}
-
 var http = require('http');
 var path = require('path')
 var express = require('express');
